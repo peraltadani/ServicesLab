6ffcd54836022a2deb105aaba0cef93104e80002
Daniel Peralta <peralta.dani@outlook.com>
2025-02-08 18:17:57 -0300

    Agrego API gateway.

---
+++ src/Services/Insumos/Insumos.DB/dbo/Tables/Deposito.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Deposito] (
+    [DepositoId]     INT           IDENTITY (1, 1) NOT NULL,
+    [NombreDeposito] VARCHAR (50)  NOT NULL,
+    [Descripcion]    VARCHAR (150) NOT NULL,
+    [Direccion]      VARCHAR (150) NOT NULL,
+    [FechaAlta]      DATETIME      NOT NULL,
+    [FechaBaja]      DATETIME      NULL,
+    CONSTRAINT [PK_Deposito] PRIMARY KEY CLUSTERED ([DepositoId] ASC)
+);
+
---
+++ src/Services/Insumos/Insumos.DB/dbo/Tables/Stock.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Stock] (
+    [StockId]                  INT             IDENTITY (1, 1) NOT NULL,
+    [InsumoId]                 INT             NOT NULL,
+    [Fecha]                    DATETIME        NOT NULL,
+    [Cantidad]                 DECIMAL (10, 2) NOT NULL,
+    [StockActual]              DECIMAL (10, 2) NOT NULL,
+    [DetalleRemitoProveedorId] INT             NULL,
+    [DepositoId]               INT             NULL,
+    CONSTRAINT [PK_Stock] PRIMARY KEY CLUSTERED ([StockId] ASC),
+    CONSTRAINT [FK_Stock_Deposito] FOREIGN KEY ([DepositoId]) REFERENCES [dbo].[Deposito] ([DepositoId]),
+    CONSTRAINT [FK_Stock_Insumo] FOREIGN KEY ([InsumoId]) REFERENCES [dbo].[Insumo] ([InsumoId])
+);
+
